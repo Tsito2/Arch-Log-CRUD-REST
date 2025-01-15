ab2ffdcf02fe7d5596f514dbfb25feff9861604f
Tsito Ramiandrisoa <ramiandrisoa@et.esiea.fr>
2025-01-15 14:16:42 +0100

    (15/01) Update BDD

--- bdd.sql
+++ bdd.sql
@@ -1,26 +1,28 @@
--- Base de données : `restaurant`
-
--- --------------------------------------------------------
-
 -- Structure de la table `orders`
-DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
                                         `id` int NOT NULL AUTO_INCREMENT,
-                                        `customerName` varchar(255) NOT NULL,
-    `status` varchar(50) DEFAULT 'pending',
-    `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-    ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
--- Structure de la table `order_items`
-DROP TABLE IF EXISTS `order_items`;
-CREATE TABLE IF NOT EXISTS `order_items` (
-                                             `id` int NOT NULL AUTO_INCREMENT,
-                                             `order_id` int DEFAULT NULL,
-                                             `name` varchar(255) DEFAULT NULL,
-    `quantity` int DEFAULT NULL,
+                                        `simulationId` varchar(255) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `arrivalDateTime` timestamp NULL DEFAULT NULL,
+    `cookStartDateTime` timestamp NULL DEFAULT NULL,
+    `cookEndDateTime` timestamp NULL DEFAULT NULL,
+    `cookBy` varchar(255) NOT NULL,
+    `deliverStartDateTime` timestamp NULL DEFAULT NULL,
+    `deliverEndDateTime` timestamp NULL DEFAULT NULL,
+    `deliverBy` varchar(255) NOT NULL,
+    `createdDateTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    KEY `order_id` (`order_id`)
-    ) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    KEY `simulationId` (`simulationId`(250))
+    ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Structure de la table `simulation`
+CREATE TABLE IF NOT EXISTS `simulation` (
+                                            `id` varchar(255) NOT NULL,
+    `ordersCountMin` int NOT NULL,
+    `ordersCountMax` int NOT NULL,
+    `startDateTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    `endDateTime` timestamp NULL DEFAULT NULL,
+    `duration` int NOT NULL,
+    `createdDateTime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3;
