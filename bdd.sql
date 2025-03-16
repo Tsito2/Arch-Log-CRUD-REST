980bf8e5149aab4d80279ceacce336633f8753c8
Thibaut Breton <contact@tbreton.fr>
2025-03-16 13:08:20 +0100

    Ajout dockerfile et modification bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE IF NOT EXISTS `restside` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;  
+USE `restside`;
 -- Table `simulation`
 CREATE TABLE IF NOT EXISTS `simulation` (
-                                            `id` VARCHAR(255) NOT NULL,
+    `id` VARCHAR(255) NOT NULL,
     `ordersCountMin` INT NOT NULL,
     `ordersCountMax` INT NOT NULL,
     `startDateTime` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
@@ -8,12 +10,12 @@ CREATE TABLE IF NOT EXISTS `simulation` (
     `duration` INT NOT NULL,
     `createdDateTime` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Table `orders`
 CREATE TABLE IF NOT EXISTS `orders` (
-                                        `id` INT NOT NULL AUTO_INCREMENT,
-                                        `simulationId` VARCHAR(255) NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `simulationId` VARCHAR(255) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
     `arrivalDateTime` TIMESTAMP NULL DEFAULT NULL,
     `cookStartDateTime` TIMESTAMP NULL DEFAULT NULL,
@@ -28,4 +30,4 @@ CREATE TABLE IF NOT EXISTS `orders` (
     CONSTRAINT `fk_orders_simulation`
     FOREIGN KEY (`simulationId`) REFERENCES `simulation` (`id`)
     ON DELETE CASCADE ON UPDATE CASCADE
-    ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
